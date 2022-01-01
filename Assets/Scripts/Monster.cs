ac95aee1c3ddb25a6839846d095df909a66ec6eb
Luis Hauenstein <luishauenstein@gmail.com>
2022-01-01 16:21:23 +0100

    implemented monster death particle emission

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -7,6 +7,9 @@ public class Monster : MonoBehaviour
 {
 
     [SerializeField] Sprite _deadSprite;
+    [SerializeField] ParticleSystem _particleSystem;
+
+    bool _hasDied = false;
 
     void OnCollisionEnter2D(Collision2D collision) {
         if(ShouldDieFromCollision(collision)) {
@@ -16,6 +19,9 @@ public class Monster : MonoBehaviour
 
     bool ShouldDieFromCollision(Collision2D collision)
     {
+        if(_hasDied)
+            return false;
+
         Bird bird = collision.gameObject.GetComponent<Bird>();
         if(bird != null) {
             return true;
@@ -29,7 +35,9 @@ public class Monster : MonoBehaviour
     }
 
     void Die() {
+        _hasDied = true;
         GetComponent<SpriteRenderer>().sprite = _deadSprite;
+        _particleSystem.Play();
         //gameObject.SetActive(false);
     }
 }
