d7574586a19e82672058eff43d2df499603a28e0
Luis Hauenstein <luishauenstein@gmail.com>
2022-01-01 17:15:16 +0100

    added c# code formatting

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -5,73 +5,80 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-    [SerializeField] float _launchForce = 500;
-    [SerializeField] float _maxDragDistance = 5;
+  [SerializeField] float _launchForce = 500;
+  [SerializeField] float _maxDragDistance = 5;
 
-    Vector2 _startPosition;
-    Rigidbody2D _rigidbody2D;
-    SpriteRenderer _spriteRenderer;
+  Vector2 _startPosition;
+  Rigidbody2D _rigidbody2D;
+  SpriteRenderer _spriteRenderer;
 
 
-    private void Awake() {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-    }
+  private void Awake()
+  {
+    _rigidbody2D = GetComponent<Rigidbody2D>();
+    _spriteRenderer = GetComponent<SpriteRenderer>();
+  }
   // Start is called before the first frame update
-    void Start()
+  void Start()
+  {
+    _startPosition = _rigidbody2D.position;
+    _rigidbody2D.isKinematic = true;
+  }
+
+  void OnMouseDown()
+  {
+    _spriteRenderer.color = Color.red;
+  }
+
+  private void OnMouseUp()
+  {
+    Vector2 currentPosition = _rigidbody2D.position;
+    Vector2 direction = _startPosition - currentPosition;
+    direction.Normalize();
+
+    _rigidbody2D.isKinematic = false;
+    _rigidbody2D.AddForce(direction * _launchForce);
+
+    _spriteRenderer.color = Color.white;
+  }
+
+  void OnMouseDrag()
+  {
+    Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+    Vector2 desiredPosition = mousePosition;
+
+    float distance = Vector2.Distance(desiredPosition, _startPosition);
+    if (distance > _maxDragDistance)
     {
-        _startPosition = _rigidbody2D.position;
-        _rigidbody2D.isKinematic = true;
+      Vector2 direction = desiredPosition - _startPosition;
+      direction.Normalize();
+      desiredPosition = _startPosition + (direction * _maxDragDistance);
     }
 
-    void OnMouseDown() {
-        _spriteRenderer.color = Color.red;
-    }
-
-    private void OnMouseUp() {
-        Vector2 currentPosition = _rigidbody2D.position;
-        Vector2 direction = _startPosition - currentPosition;
-        direction.Normalize();
-
-        _rigidbody2D.isKinematic = false;
-        _rigidbody2D.AddForce(direction * _launchForce);
-
-        _spriteRenderer.color = Color.white;
-    }
-
-    void OnMouseDrag() {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        Vector2 desiredPosition = mousePosition;
-
-        float distance = Vector2.Distance(desiredPosition, _startPosition);
-        if(distance> _maxDragDistance) {
-            Vector2 direction = desiredPosition - _startPosition;
-            direction.Normalize();
-            desiredPosition = _startPosition + (direction * _maxDragDistance);
-        }
-
-        if(desiredPosition.x > _startPosition.x) {
-            desiredPosition.x = _startPosition.x;
-        }
-
-        _rigidbody2D.position = desiredPosition;
-    }
-    // Update is called once per frame
-    void Update()
+    if (desiredPosition.x > _startPosition.x)
     {
-        
+      desiredPosition.x = _startPosition.x;
     }
 
-    void OnCollisionEnter2D(Collision2D collision) {
-        StartCoroutine(ResetAfterDelay());
-    }
+    _rigidbody2D.position = desiredPosition;
+  }
+  // Update is called once per frame
+  void Update()
+  {
 
-    IEnumerator ResetAfterDelay()
-    {
-        yield return new WaitForSeconds(3);
-        _rigidbody2D.position = _startPosition;
-        _rigidbody2D.isKinematic = true;
-        _rigidbody2D.velocity = Vector2.zero;
-    }
+  }
+
+  void OnCollisionEnter2D(Collision2D collision)
+  {
+    StartCoroutine(ResetAfterDelay());
+  }
+
+  IEnumerator ResetAfterDelay()
+  {
+    yield return new WaitForSeconds(3);
+    _rigidbody2D.position = _startPosition;
+    _rigidbody2D.isKinematic = true;
+    _rigidbody2D.velocity = Vector2.zero;
+  }
 }
--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -1,40 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelController : MonoBehaviour
 {
-    [SerializeField] string _nextLevelName;
-    private Monster[] _monsters;
+  [SerializeField] string _nextLevelName;
+  private Monster[] _monsters;
 
-    void OnEnable() {
-        _monsters = FindObjectsOfType<Monster>();
-    }
+  void OnEnable()
+  {
+    _monsters = FindObjectsOfType<Monster>();
+  }
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (MonstersAreAllDead())
+      GoToNextLevel();
+  }
+
+  bool MonstersAreAllDead()
+  {
+    foreach (var monster in _monsters)
     {
-        
+      if (monster.gameObject.activeSelf)
+        return false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(MonstersAreAllDead())
-            GoToNextLevel();
-    }
+    return true;
+  }
 
-    bool MonstersAreAllDead() {
-        foreach (var monster in _monsters)
-        {
-            if(monster.gameObject.activeSelf)
-                return false;
-        }
-
-        return true;
-    }
-
-    void GoToNextLevel() {
-        Debug.Log("Go to level " + _nextLevelName);
-    }
+  void GoToNextLevel()
+  {
+    Debug.Log("Go to level " + _nextLevelName);
+    SceneManager.LoadScene(_nextLevelName);
+  }
 }
